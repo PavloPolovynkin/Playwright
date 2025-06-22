69ac051c9aaca5572d503ae485b1a324dcef0f79
repo_fro_test <pavlopolovinkin@gmail.com>
2025-06-22 21:00:19 +0300

    hw25_CLI_and_Environment_variables

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,10 @@ import { defineConfig, devices } from '@playwright/test';
 /* Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 // import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+//dotenv.config({ path: path.resolve(__dirname, '.env') });
+require('dotenv').config();
 
 /* See https://playwright.dev/docs/test-configuration.
  */
@@ -31,10 +32,10 @@ export default defineConfig({
     trace: 'on',
     video: 'on',
     screenshot: 'on',
-    baseURL: 'https://qauto.forstudy.space/',
+    baseURL: process.env.BASE_URL,
     httpCredentials: {
-      'username': 'guest',
-      'password': 'welcome2qauto'
+      'username': process.env.HTTP_CREDENTIALS_USERNAME!, 
+      'password': process.env.HTTP_CREDENTIALS_PASSWORD! 
     },
     headless: false, // ← запускає браузер з вікном
   },
@@ -84,3 +85,5 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
+
+// export default config
